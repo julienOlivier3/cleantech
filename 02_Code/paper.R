5dd87253ab40005a82dce4566963cd85e61f353f
julienOlivier3 <julian-doerr@gmx.de>
2022-04-01 20:34:31 +0200

    automated R code for paper analyses

--- 02_Code/paper.R
+++ 02_Code/paper.R
@@ -1,51 +1,117 @@
 # Setup -------------------------------------------------------------------
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(here, labelled, hablar, MASS, tidyverse, VIM, Hmisc, psych, margins, pscl, xtable)
+pacman::p_load(here, labelled, hablar, MASS,VIM, Hmisc, psych, margins, pscl, xtable, tidyverse)
 source(here("02_Code/util.R"))
 
 
 
 # Functions ---------------------------------------------------------------
-# Function to extract relevant info from model
-clean_results <- function(model, ndigits=3){
+
+# Function to return publication ready coefficient estimates
+clean_digit <- function(digit, ndigits_display=3, ndigits_round=3){
+  return(sprintf(paste0("%.", ndigits_display, "f"), round(digit, ndigits_round)))
+}
+
+# Function to extract relevant info from ordered Logit model
+clean_results <- function(model, ndigits_display=3, ndigits_round=3){
   res <- summary(model)$coefficients %>% 
     as_tibble(rownames = "variable") %>% 
     mutate(
       odds = map_dbl(Value, function(x) exp(x)),
       p_value = map_dbl(`t value`, function(x) round(pnorm(abs(x), lower.tail = FALSE) * 2, 6))) %>% 
     mutate(odds_p = case_when(
-      p_value < 0.01 ~ paste0(round(odds, ndigits), "***"),
-      p_value < 0.05 & p_value >= 0.01 ~ paste0(round(odds, ndigits), "**"),
-      p_value < 0.1 & p_value >= 0.05 ~ paste0(round(odds, ndigits), "*"),
-      p_value >= 0.1 ~ paste0(round(odds, ndigits))
+      p_value < 0.01 ~ paste0(clean_digit(odds, ndigits_display, ndigits_round), "***"),
+      p_value < 0.05 & p_value >= 0.01 ~ paste0(clean_digit(odds, ndigits_display, ndigits_round), "**"),
+      p_value < 0.1 & p_value >= 0.05 ~ paste0(clean_digit(odds, ndigits_display, ndigits_round), "*"),
+      p_value >= 0.1 ~ paste0(clean_digit(odds, ndigits_display, ndigits_round))
     )) %>% 
     select(variable, odds_p) %>% 
     bind_rows(tibble(variable = "N", odds_p = as.character(nobs(model)))) %>% 
-    bind_rows(tibble(variable = "BIC", odds_p = as.character(round(BIC(model), ndigits)))) %>% 
-    bind_rows(tibble(variable = "Pseudo_R2", odds_p = as.character(round(pR2(model)['r2ML'], ndigits)))) 
+    bind_rows(tibble(variable = "BIC", odds_p = as.character(clean_digit(BIC(model), ndigits_display, ndigits_round)))) %>% 
+    bind_rows(tibble(variable = "Pseudo_R2", odds_p = as.character(clean_digit(pR2(model)['McFadden'], ndigits_display, ndigits_round)))) 
   
   return(res)
 }
 
-clean_results_logit <- function(model, ndigits=3){
+# Function to extract relevant info from Logit model
+clean_results_logit <- function(model, ndigits_display=3, ndigits_round=3){
   helper <- summary(model)$coefficients %>% as_tibble(rownames = "factor") %>% select(factor) %>% filter(!(factor == "(Intercept)")) %>% as_vector()
   
   res <- margins_summary(model) %>% 
     as_tibble() %>% 
     mutate(ame_p = case_when(
-      p < 0.01 ~ paste0(round(AME, ndigits), "***"),
-      p < 0.05 & p >= 0.01 ~ paste0(round(AME, ndigits), "**"),
-      p < 0.1 & p >= 0.05 ~ paste0(round(AME, ndigits), "*"),
-      p >= 0.1 ~ paste0(round(AME, ndigits))
+      p < 0.01 ~ paste0(clean_digit(AME, ndigits_display, ndigits_round), "***"),
+      p < 0.05 & p >= 0.01 ~ paste0(clean_digit(AME, ndigits_display, ndigits_round), "**"),
+      p < 0.1 & p >= 0.05 ~ paste0(clean_digit(AME, ndigits_display, ndigits_round), "*"),
+      p >= 0.1 ~ paste0(clean_digit(AME, ndigits_display, ndigits_round))
     )) %>% 
     select(factor, ame_p) %>% 
     arrange(helper) %>% 
     bind_rows(tibble(factor = "N", ame_p = as.character(nobs(model)))) %>% 
-    bind_rows(tibble(factor = "BIC", ame_p = as.character(round(BIC(model), ndigits)))) %>% 
-    bind_rows(tibble(factor = "Pseudo_R2", ame_p = as.character(round(pR2(model)['r2ML'], ndigits)))) 
+    bind_rows(tibble(factor = "BIC", ame_p = as.character(clean_digit(BIC(model), ndigits_display, ndigits_round)))) %>% 
+    bind_rows(tibble(factor = "Pseudo_R2", ame_p = as.character(clean_digit(pR2(model)['McFadden'], ndigits_display, ndigits_round)))) 
   
   return(res)
 }
+
+
+# Function to clean up regression results and to produce nice regression tables
+create_reg_tab <- function(df_res, all_sector=TRUE, all_product=FALSE){
+
+  df_bottom <- df_res %>% filter(variable %in% bottom)
+  df_top <- df_res %>% filter(!(variable %in% bottom))
+  if (all_sector){
+    sector_controls <- rep("Y", length(df_top)-1)
+  }
+  else{
+    sector_controls <- rep("N", length(df_top)-1)
+  }
+  if (all_product){
+    product_controls <- rep("Y", length(df_top)-1)
+  }
+  else{
+    product_controls <- c(rep("N", length(df_top)-2), "Y")
+  }
+  
+  
+  df_controls <- tibble(`Sector controls` = sector_controls,
+                        `Product type controls` = product_controls) %>% 
+    t() %>% 
+    as_tibble(rownames = "variable")
+  colnames(df_controls) <- colnames(df_top) 
+  
+  
+  df_res <- df_top %>% 
+    bind_rows(df_controls) %>% 
+    bind_rows(df_bottom) 
+  
+  df_latex <- df_res %>% 
+    filter(!str_detect(variable, "branche.{1,}")) %>% 
+    filter(!str_detect(variable, "produkt.{1,}")) %>% 
+    filter(!str_detect(variable, "ja")) %>% 
+    filter(!str_detect(variable, "BIC")) %>% 
+    mutate(variable = case_when(
+      variable == 'tech_prox' ~ '\\textsc{TechProx}$_{max}$',
+      variable == 'tech_prox_t' ~ '\\textsc{TechProx}$_{t}$',
+      variable == 'cleantech' ~ '\\textsc{CleanTech}',
+      variable == 'cleantech_t' ~ '\\textsc{CleanTech}$_{t}$',
+      (variable == 'bes') | (variable == 'log(bes)') ~ 'log(size)',
+      variable == 'fue' ~ 'R\\&D',
+      variable == 'fuep_bes' ~ 'R\\&D intensity',
+      variable == 'umsj' ~ 'returns',
+      variable == 'gewj' ~ 'break even',
+      variable == 'foe' ~ 'subsidy',
+      variable == 'anzteam' ~ 'team size',
+      variable == 'uni' ~ 'university',
+      variable == 'N' ~ '$N$',
+      variable == 'Pseudo_R2' ~ 'Pseudo $R^2$',
+      TRUE ~ variable)
+    )
+  
+  return(df_latex)
+    
+}
+
 
 
 # Read data ---------------------------------------------------------------
@@ -59,7 +125,7 @@ cleantech_fields <- c("Adaption", "Battery", "Biofuels", "CCS", "E_Efficiency", 
 
 # Select relevant columns for regression
 df_model <- df_startup %>% 
-  mutate(age = 2018 - gr_jahr) %>% 
+  mutate(age = 2017 - gr_jahr) %>% 
   mutate(foe = ifelse(rowSums(select(., foe_ba, foe_kfw, foe_land, foe_kokr, foe_bueba, foe_bund, foe_eu))>0, 1, 0)) %>% 
   mutate(ums_hilf = ifelse(ums==0, 1, ums)) %>% 
   mutate(bes_hilf = rowSums(select(., anzteam, bes_l, bes_m, bes_h), na.rm = TRUE),
@@ -67,6 +133,8 @@ df_model <- df_startup %>%
          bes = ifelse(bes_hilf < fuep, fuep, bes_hilf)) %>% 
   mutate(fuek_ums = fuek/ums_hilf,
          fuep_bes = fuep/bes) %>% 
+  mutate(uni = ifelse(a_uni == 1 | a_dr == 1, 1, 0)) %>% 
+  mutate(gewj = ifelse(gewj == 1 | gewj == 3, 1, 0)) %>% 
   mutate_at(vars(contains("branche11")), function(x) haven::as_factor(x, levels="label")) %>% 
   mutate_at(vars(contains("produkt")), function(x) haven::as_factor(x, levels="label")) %>% 
   mutate_at(vars(contains("umwelt")), function(x) haven::as_factor(x, levels="label")) %>% 
@@ -99,7 +167,9 @@ df_model <- df_startup %>%
   mutate(umwelt_wirk = sum(c_across(umwelt_wirk_gesverbr:umwelt_wirk_haltbar), na.rm = FALSE),
          umwelt_inno = sum(c_across(umwelt_inno_gesverbr:umwelt_inno_haltbar), na.rm = FALSE)) %>% 
   ungroup() %>% 
-  select(gpkey, age, produkt_kat, branche11, team, anzteam, foe, bes, fue, fuep, fuek_ums, fuep_bes, fuex, fuek, patj, anzpat, umsj, ums, gewj, gew, contains("umwelt_wirk"), contains("umwelt_inno"), cleantech_fields, url, text_en)
+  select(gpkey, age, produkt_kat, branche11, team, anzteam, uni, foe, bes, 
+         fue, fuep, fuek_ums, fuep_bes, fuex, fuek, patj, anzpat, umsj, ums, gewj, gew, 
+         contains("umwelt_wirk"), contains("umwelt_inno"), cleantech_fields, url, web, text_en)
 
 # Create technology column
 df_tech <- df_startup %>% 
@@ -166,7 +236,7 @@ df_model %>%
 # Some simple kNN imputation
 df_model <- kNN(df_model, 
                 metric = "mahalanobis", 
-                variable = c("team", "anzteam", "fue", "fuep", "umsj", "foe", "bes", "fuep_bes", "fuek_ums"), 
+                variable = c("team", "anzteam", "fue", "fuep", "umsj", "foe", "bes", "fuep_bes", "fuek_ums", "uni"), 
                 dist_var = c("age", "branche11", "team", "bes", "fue", "umsj"), 
                 k=10, 
                 imp_var = FALSE, 
@@ -182,12 +252,17 @@ df_model %>%
 desc_list <- vector(mode = "list", length = 2)
 names(desc_list) <- c("Hmisc", "psych")
 
-desc_list[[1]] <- apply(df_model %>% select(cleantech, tech_prox, foe, bes, fuek_ums, fuep_bes, fue, fuep, fuex, fuek, patj, anzpat, umsj, ums, gewj, gew, age, team, anzteam, cleantech_fields, contains("umwelt")), MARGIN = 2, function(x) Hmisc::describe(x))
-desc_list[[2]] <- sapply(df_model %>% select(cleantech, tech_prox, foe, bes, fuek_ums, fuep_bes, fue, fuep, fuex, fuek, umsj, ums, gewj, gew, age, team, anzteam, cleantech_fields, 
-                                             umwelt_wirk_gesverbr:umwelt_wirk_haltbar, umwelt_wirk, umwelt_inno_gesverbr:umwelt_inno_haltbar, umwelt_inno), function(x) psych::describe(x))
+#desc_list[[1]] <- apply(df_model %>% select(tech_prox, cleantech, bes, age, foe, fue, fuep_bes, fuek_ums, umsj, ums, gewj, gew, team, anzteam, uni), MARGIN = 2, function(x) Hmisc::describe(x))
+desc_list[[2]] <- sapply(df_model %>% select(tech_prox, cleantech, bes, age, fue, fuep_bes, umsj, gewj, foe, anzteam, uni), function(x) psych::describe(x))
 
+desc_list$psych %>% 
+  as_tibble(rownames = "Variable") %>% 
+  mutate_all(function(x) unlist(x)) %>% 
+  filter(Variable %in% c("mean", "sd", "min", "max", "n")) %>% 
+  pivot_longer(tech_prox:uni) %>% 
+  pivot_wider(id_cols = Variable:value, names_from=Variable)
 
-saveRDS(desc_list, file=here("01_Data/02_Firms/03_StartupPanel/desc_list.rds"))
+#saveRDS(desc_list, file=here("01_Data/02_Firms/03_StartupPanel/desc_list.rds"))
 
 # Correlations ------------------------------------------------------------
 corr_list <- vector(mode = "list", length = 2)
@@ -196,76 +271,66 @@ names(corr_list) <- c("pearson", "spearman")
 corr_list[[1]] <- rcorr(df_model %>% select(umwelt_inno, umwelt_wirk, cleantech, tech_prox, foe, bes, fuek_ums, fuep_bes, fue, fuep, fuex, fuek, patj, anzpat, umsj, ums, gewj, gew, age, anzteam, cleantech_fields) %>% as.matrix(), type = "pearson")
 corr_list[[2]] <- rcorr(df_model %>% select(umwelt_inno, umwelt_wirk, cleantech, tech_prox, foe, bes, fuek_ums, fuep_bes, fue, fuep, fuex, fuek, patj, anzpat, umsj, ums, gewj, gew, age, anzteam, cleantech_fields) %>% as.matrix(), type = "spearman")
 
-saveRDS(corr_list, file=here("01_Data/02_Firms/03_StartupPanel/corr_list.rds"))
+
+
+#saveRDS(corr_list, file=here("01_Data/02_Firms/03_StartupPanel/corr_list.rds"))
 
 # Regression analyses -----------------------------------------------------
-## Ordered Logit (partial) ================================================
+## Ordered Logit (full) ===================================================
 ### Umweltwirkung #########################################################
-
-model_equations <- lapply(cleantech_fields, function(tech) paste("umwelt_wirk_cat ~", tech, "+ branche11 + produkt_kat")) %>% as_vector()
-
 
 ## TechProx
 
-df_model2 <- df_model %>% 
+df_reg <- df_model %>% 
   mutate_at(cleantech_fields, function(x) x*100) 
-
-
-
-
-
-model_list <- vector(mode = "list", length = length(model_equations))
-names(model_list) <- model_equations
-result_table <- tibble(
-  model = character()
-)
-
-for (i in seq_along(model_equations)){
-  model_equation <- model_equations[i]
-  print(model_equation)
-  
-  polr_mod <- polr(formula = as.formula(model_equation), data = df_model2, na.action = na.omit)
-  model_list[[i]] <- polr_mod
-  
-  coef_results <- clean_results(polr_mod, variables = c(cleantech_fields, str_detect()))
-  result_table <- result_table %>% bind_rows(tibble(model = model_equation, coef_results))
-  
+df_res <- tibble()
+bottom <- c("N", "BIC", "Pseudo_R2")
+for (i in seq_along(cleantech_fields)){
+  tech <- cleantech_fields[i]
+  model_equation <- paste("umwelt_wirk_cat ~", tech, "+ branche11 + log(bes) + age + fue + fuep_bes + foe + umsj + gewj + anzteam + uni + produkt_kat")
+  polr_mod <- polr(formula = as.formula(model_equation), data = df_reg, na.action = na.omit, method = "logistic")
+  df_temp <- clean_results(model = polr_mod)
+  colnames(df_temp)  <- c("variable", tech)
+  df_temp <- df_temp %>% mutate(variable = ifelse(variable == tech, "tech_prox_t", variable))
+  if(i == 1){
+    df_res <- df_temp
+  }
+  else{
+    df_res <- df_res %>% left_join(df_temp, by = "variable")
+  }
 }
 
+df_latex <- create_reg_tab(df_res, all_sector = TRUE, all_product = TRUE)
+df_latex
 
-result_table %>% 
-  filter(!str_detect(variable, "branche.{1,}")) %>% 
-  filter(!str_detect(variable, "produkt.{1,}")) %>% 
-  filter(!str_detect(variable, "ja")) 
+print.xtable(df_latex, type = "latex")
 
 
 ## cleantech 
 
-df_model3 <- df_model %>% 
+df_reg <- df_model %>% 
   mutate(across(cleantech_fields, function(x) ifelse(x > th, 1, 0)))
-
-model_list <- vector(mode = "list", length = length(model_equations))
-names(model_list) <- model_equations
-result_table <- tibble(
-  model = character()
-)
-
-for (i in seq_along(model_equations)){
-  model_equation <- model_equations[i]
-  print(model_equation)
-  
-  polr_mod <- polr(formula = as.formula(model_equation), data = df_model3, na.action = na.omit)
-  model_list[[i]] <- polr_mod
-  
-  coef_results <- clean_results(polr_mod, variables = c(cleantech_fields, "fue", "anzteam", "age"))
-  result_table <- result_table %>% bind_rows(tibble(model = model_equation, coef_results))
-  
+df_res <- tibble()
+bottom <- c("N", "BIC", "Pseudo_R2")
+for (i in seq_along(cleantech_fields)){
+  tech <- cleantech_fields[i]
+  model_equation <- paste("umwelt_wirk_cat ~", tech, "+ branche11 + log(bes) + age + fue + fuep_bes + foe + umsj + gewj + anzteam + uni + produkt_kat")
+  polr_mod <- polr(formula = as.formula(model_equation), data = df_reg, na.action = na.omit, method = "logistic")
+  df_temp <- clean_results(model = polr_mod)
+  colnames(df_temp)  <- c("variable", tech)
+  df_temp <- df_temp %>% mutate(variable = ifelse(variable == tech, "cleantech_t", variable))
+  if(i == 1){
+    df_res <- df_temp
+  }
+  else{
+    df_res <- df_res %>% left_join(df_temp, by = "variable")
+  }
 }
 
-result_table %>% 
-  filter(!str_detect(variable, "branche.{1,}")) %>% 
-  filter(!str_detect(variable, "produkt.{1,}")) %>% 
-  filter(!str_detect(variable, "ja")) 
+df_latex <- create_reg_tab(df_res, all_sector = TRUE, all_product = TRUE)
+df_latex
+
+print.xtable(df_latex, type = "latex")
 
 ## Ordered Logit (full) ===================================================
 ### Umweltwirkung #########################################################
@@ -273,17 +338,16 @@ result_table %>%
 
 model_equations <- c(  
   "umwelt_wirk_cat ~ tech_prox + branche11",
-  "umwelt_wirk_cat ~ tech_prox + branche11 + log(bes)",
+  #"umwelt_wirk_cat ~ tech_prox + branche11 + log(bes)",
   "umwelt_wirk_cat ~ tech_prox + branche11 + log(bes) + age",
-  "umwelt_wirk_cat ~ tech_prox + branche11 + log(bes) + age + fue",
-  #"umwelt_wirk_cat ~ tech_prox + branche11 + log(bes) + age + scale(fuek_ums)",
-  #"umwelt_wirk_cat ~ tech_prox + branche11 + log(bes) + age + fuep_bes",
-  "umwelt_wirk_cat ~ tech_prox + branche11 + log(bes) + age + log(fuek + 1)",
-  "umwelt_wirk_cat ~ tech_prox + branche11 + log(bes) + age + fue + foe",
-  "umwelt_wirk_cat ~ tech_prox + branche11 + log(bes) + age + fue + foe + umsj",
-  "umwelt_wirk_cat ~ tech_prox + branche11 + log(bes) + age + fue + foe + log(ums + 1)",
-  "umwelt_wirk_cat ~ tech_prox + branche11 + log(bes) + age + fue + foe + umsj + gewj",
-  "umwelt_wirk_cat ~ tech_prox + branche11 + log(bes) + age + fue + foe + umsj + gewj + produkt_kat"
+  #"umwelt_wirk_cat ~ tech_prox + branche11 + log(bes) + age + fue",
+  #"umwelt_wirk_cat ~ tech_prox + branche11 + log(bes) + age + fue + fuep_bes",
+  "umwelt_wirk_cat ~ tech_prox + branche11 + log(bes) + age + fue + fuep_bes + foe",
+  #"umwelt_wirk_cat ~ tech_prox + branche11 + log(bes) + age + fue + fuep_bes + foe + umsj",
+  "umwelt_wirk_cat ~ tech_prox + branche11 + log(bes) + age + fue + fuep_bes + foe + umsj + gewj",
+  #"umwelt_wirk_cat ~ tech_prox + branche11 + log(bes) + age + fue + fuep_bes + foe + umsj + gewj + anzteam",
+  "umwelt_wirk_cat ~ tech_prox + branche11 + log(bes) + age + fue + fuep_bes + foe + umsj + gewj + anzteam + uni",
+  "umwelt_wirk_cat ~ tech_prox + branche11 + log(bes) + age + fue + fuep_bes + foe + umsj + gewj + anzteam + uni + produkt_kat"
 )
 
 df_reg <- df_model %>% 
@@ -299,23 +363,18 @@ for (i in seq_along(model_equations)){
     df_res <- df_temp
   }
   else{
-    df_res <- df_res %>% full_join(df_temp, by = "variable", )
+    df_res <- df_res %>% full_join(df_temp, by = "variable")
   }
 }
 
-df_bottom <- df_res %>% filter(variable %in% bottom)
-df_top <- df_res %>% filter(!(variable %in% bottom))
+df_latex <- create_reg_tab(df_res)
+df_latex
 
-df_res <- df_top %>% 
-  bind_rows(df_bottom)
-
-
-df_latex <- df_res %>% 
-  filter(!str_detect(variable, "branche.{1,}")) %>% 
-  filter(!str_detect(variable, "produkt.{1,}")) %>% 
-  filter(!str_detect(variable, "ja"))
-
-print.xtable(df_latex, type = "latex")
+print.xtable(df_latex, 
+             type = "latex", 
+             #include.rownames = FALSE,
+             #floating = FALSE, 
+             format.args = list(digits = 3, big.mark = " ", decimal.mark = ","))
 
 
 
@@ -370,7 +429,7 @@ model_equations <- c(
   "umwelt_inno_cat ~ tech_prox + branche11 + log(bes) + age + fue + foe + umsj",
   "umwelt_inno_cat ~ tech_prox + branche11 + log(bes) + age + fue + foe + log(ums + 1)",
   "umwelt_inno_cat ~ tech_prox + branche11 + log(bes) + age + fue + foe + umsj + gewj",
-  "umwelt_inno_cat ~ tech_prox + branche11 + log(bes) + age + fue + foe + umsj + gewj + produkt_kat"
+  "umwelt_inno_cat ~ tech_prox + branche11 + log(bes) + age + fue + foe + umsj + gewj + produkt_kat + uni"
 )
 
 df_reg <- df_model %>% 
@@ -401,6 +460,7 @@ df_latex <- df_res %>%
   filter(!str_detect(variable, "branche.{1,}")) %>% 
   filter(!str_detect(variable, "produkt.{1,}")) %>% 
   filter(!str_detect(variable, "ja"))
+df_latex
 
 print.xtable(df_latex, type = "latex")
 
